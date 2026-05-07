2a528d938d50aa036130b4ee695db7d12358d0d2
Viktor Semenov <mail@sobix.net>
2026-05-07 16:37:44 +0300

    add

--- include/model/ReportAccount.hpp
+++ include/model/ReportAccount.hpp
@@ -64,7 +64,7 @@ struct ReportAccountRecord {
     time_t lastdate = 0;
     int leverage = 0;
     int agent_account = 0;
-    time_t timestamp = 0;
+    time_t update_time = 0;
     int last_ip = 0;
     double balance = 0.0;
     double prevmonthbalance = 0.0;
